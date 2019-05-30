4ee11f6a7bc8a67509477ce76dd406b2336286b4
Gabriel Leocadio <gabrielsilva94@gmail.com>
2019-05-30 14:32:33 -0300

    Commit inicial

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <ProductStock/>
+  </div>
+</template>
+
+<script>
+import ProductStock from './components/ProductStock'
+
+export default {
+  name: 'App',
+  components: {
+    ProductStock
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
